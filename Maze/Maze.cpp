d6df8a81857a3db38b06450ca9d09f16054ab3d7
Marcin Nowak <SourVenom1994@gmail.com>
2022-10-25 21:58:32 +0200

    Extracted DrawMap, added currentlevel, added tiles to Position

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -17,15 +17,21 @@ void sleep(float seconds) {
 struct Score {
 	int level = 0;
 	int score = 0;
-	std::string nickname = {};
+	std::string name = {};
 };
 struct position {
 	unsigned int level = {};
 	unsigned int row = {};
 	unsigned int column = {};
+	
+	char* tileup = {};
+	char* tiledown = {};
+	char* tileleft = {};
+	char* tileright = {};
 };
+
 bool Highscore(const Score & player_score, std::string& gamepath) {
-	std::cout << '\n' << player_score.nickname << "\'s score:\n"
+	std::cout << '\n' << player_score.name << "\'s score:\n"
 		<< "Levels passed:\t" << player_score.level << '\n'
 		<< "Points:\t\t" << player_score.score << '\n\n';
 	
@@ -39,7 +45,7 @@ bool Highscore(const Score & player_score, std::string& gamepath) {
 	};
 	saved_score >> current_highscore.level;
 	saved_score >> current_highscore.score;
-	saved_score >> current_highscore.nickname;
+	saved_score >> current_highscore.name;
 	saved_score.close();
 
 	if (current_highscore.level>= player_score.level && 
@@ -53,7 +59,7 @@ bool Highscore(const Score & player_score, std::string& gamepath) {
 	};
 
 	std::ofstream save_score(savefilename);
-	save_score << player_score.level << "\n" << player_score.score << "\n" << player_score.nickname;
+	save_score << player_score.level << "\n" << player_score.score << "\n" << player_score.name;
 	save_score.close();
 	std::cout << "Your score has been saved!:\a\n";
 
@@ -65,55 +71,72 @@ void DisplayScore(std::string& filename) {
 	std::ifstream Scores("Tabela_wynikow.txt");
 	Scores >> Score.level;
 	Scores >> Score.score;
-	Scores >> Score.nickname;
+	Scores >> Score.name;
 	Scores.close();
 	std::cout << "Nick\t|\tLevels passed\t|\tScore\t|\t\n"
 		<< "---------------------------------------------------\n"
-		<< Score.nickname << "\t\t" << Score.level << "\t\t\t" << Score.score << "\n\n"
+		<< Score.name << "\t\t" << Score.level << "\t\t\t" << Score.score << "\n\n"
 		<< "Press any key to continue ...\n";
 }
-int Game(std::vector<std::vector<std::vector<char>>> map, std::string& gamepath) {
-	Score current_score = {};
-	std::cout << "Input Your nickname: ";
-	std::getline(std::cin, current_score.nickname);
-	unsigned int levelscore = 0;
+std::string& DrawMap(
+	std::vector<std::vector<char>> map,
+	position& hero, 
+	position& exit,
+	int sessionscore,
+	int levelscore,
+	std::string& buffer) {
+	for (size_t row = 0; row < map.size(); row++) {
+		for (size_t column = 0; column < map[row].size(); column++) {
+
+			buffer += (map[row][column] == WALL ? char(219) : map[row][column]);
+			switch (map[row][column]) {
+			case HERO: {
+				hero.row = row;
+				hero.column = column; }; break;
+			case EXIT: {
+				exit.row = row;
+				exit.column = column; }; break;
+			default:
+				break;
+			}
+		};
+		buffer += ('\n');
+	};
+	buffer += "Current score: "
+		+ std::to_string(sessionscore)
+		+ "\tLevel score: "
+		+ std::to_string(levelscore) + '\n';
+	return buffer;
+};
+int Game(std::vector<std::vector<std::vector<char>>> map, std::string& gamepath, Score current_score) {
+	int levelscore = -1;
 	position hero;
+	position exit;
 	char *herotile = NULL;
 	char *destinationtile = NULL;
 	char *exittile = NULL;
 	char move = {};
-	std::string buffer{};
+	
 	
 	//Game loop ------------------------------
 	Solver solver;
 
 	cls(hStdout);
 	for (size_t level = 0; level < map.size(); level++) {
+		std::vector<std::vector<char>> currentlevel = map[level];
 		current_score.level++;
-		levelscore = 0;
+		hero.level = exit.level = level;
 		do{
+			cls(hStdout);
+			std::string buffer{};
 			//Draw map
-			for (size_t row = 0; row < map[level].size(); row++) {
-				for (size_t column = 0; column < map[level][row].size(); column++) {
-					
-					std::cout << (map[level][row][column] == WALL ? char(219) : map[level][row][column]);
-					//buffer.append(map[level][row][column] == WALL ? char(219) : map[level][row][column]);
-					switch (map[level][row][column]) {
-					case 'H': { herotile = &map[level][row][column]; 
-								hero.level = level; 
-								hero.row = row;
-								hero.column = column; }; break;
-					case 'O': { exittile = &map[level][row][column]; }; break;
-
-					default:
-						break;
-					}
-				};
-				std::cout << '\n';
-				//buffer.append('\n');
-			};
-			//std::cout << buffer;
-			std::cout << "Current score: " << current_score.score << "\tLevel score: " << levelscore << '\n';
+			DrawMap(currentlevel,
+				hero, 
+				exit, 
+				current_score.score, 
+				levelscore++, 
+				buffer);
+			std::cout << buffer;
 			
 			//Move
 			
@@ -127,27 +150,35 @@ int Game(std::vector<std::vector<std::vector<char>>> map, std::string& gamepath)
 			
 			//move = _getch();
 			
-			do {
-				sleep(1);
-				solver.rotate(movLEFT);		//check left
-				move = solver.advance();	//chek if wall
+			//do {
+				//sleep(1);
+				//solver.rotate(movLEFT);		//check left
+				herotile = &currentlevel[hero.row][hero.column];
+				hero.tileup = &currentlevel[hero.row - 1][hero.column];
+				hero.tiledown = &currentlevel[hero.row + 1][hero.column];
+				hero.tileleft = &currentlevel[hero.row][hero.column - 1];
+				hero.tileright = &currentlevel[hero.row][hero.column + 1];
+				exittile= &map[exit.level][exit.row][exit.column];
+				
+				move = _getch(); //= solver.advance();	//chek if wall
 				switch (move) {
-				case LEFT: {destinationtile = &map[hero.level][hero.row][hero.column - 1]; }; break;
-				case RIGHT: {destinationtile = &map[hero.level][hero.row][hero.column + 1]; }; break;
-				case DOWN: {destinationtile = &map[hero.level][hero.row + 1][hero.column]; }; break;
-				case UP: {destinationtile = &map[hero.level][hero.row - 1][hero.column]; }; break;
+				case LEFT: {destinationtile = hero.tileleft; }; break;
+				case RIGHT: {destinationtile = hero.tileright; }; break;
+				case DOWN: {destinationtile = hero.tiledown; }; break;
+				case UP: {destinationtile = hero.tileup; }; break;
 				case ESC: {std::cout << "Are you sure you want to exit? Y/N : ";
 					if (_getch() == 'y' || _getch() == 'Y') return EXIT_SUCCESS; }; break;
 				default: {std::cout << "\nMove not defined!\a\n"; _getch(); }; break;
 				}
-				if (solver.facingWall((tiles)*destinationtile)) { solver.rotate(movRIGHT); };
-			} while (solver.facingWall(tiles(*destinationtile)));
+				
+				//if (solver.facingWall((tiles)*destinationtile)) { solver.rotate(movRIGHT); };
+			//} while (solver.facingWall(tiles(*destinationtile)));
+
 			//if (move == '0' || move == '0xE0') { move = _getch(); };	//arrow keys are precieded either by 0 or 0xE0 in input
 			
 			if (*destinationtile != CORRIDOR) { std::cout << '\a'; continue; };
 			std::swap(*herotile, *destinationtile);
 			levelscore++;
-			cls(hStdout);
 		} while (!(destinationtile==exittile));
 		current_score.score += levelscore;
 		cls(hStdout);
@@ -182,7 +213,7 @@ int PrepareMap(const unsigned int & width, const unsigned int & height, std::str
 			_getch();
 			return EXIT_FAILURE;
 	};
-	////////////////////////////////////////////////////////////////////////Wrzucanie mapy z pliku do tablicy tab
+	//Read map
 	
 	for (size_t level = 0; level < amount_of_levels; level++){
 		full_level.clear();
@@ -204,7 +235,12 @@ int PrepareMap(const unsigned int & width, const unsigned int & height, std::str
 		if (map_file.peek() == EOF) { break; }
 	};
 	map_file.close();
-	Game(map, gamepath);
+
+	Score score = {};
+	std::cout << "Input Your nickname: ";
+	std::getline(std::cin, score.name);
+
+	Game(map, gamepath, score);
 	return EXIT_SUCCESS;
 };
 
